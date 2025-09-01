d6d41bbea4b8f8d245ba3c0405bb9c0a28ffc61c
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2025-09-01 13:45:20 -0300

    add: Primeira versão do projeto individual em java
    Gerenciador de Tarefas

---
+++ Henrique/src/main/java/flow/infra/GerenciadorDeTarefas.java
@@ -0,0 +1,55 @@
+package flow.infra;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GerenciadorDeTarefas {
+
+    public List<String> cadastrarTarefas() {
+        List<String> tarefas = new ArrayList<>();
+        Integer contador = 0;
+        while (!contador.equals(1)) {
+
+            Scanner leitorTexto = new Scanner(System.in);
+            Scanner leitorNumero = new Scanner(System.in);
+
+            System.out.println();
+            System.out.print("""
+                    GERENCIADOR DE TAREFAS
+                    ----------------------
+                    MENU:
+                    - 1. Cadastrar Tarefas
+                    - 2. Listar Tarefas
+                    - 3. Concluir Tarefa
+                    ----------------------
+                    """);
+            System.out.println();
+            System.out.print("Escolha: ");
+            Integer escolha = leitorNumero.nextInt();
+            if (escolha != 1 && escolha != 2 && escolha != 3) {
+                System.out.println("Informe um valor correspondente ao menu!");
+            }
+            if (escolha == 1) {
+                System.out.print("Qual tarefa deseja cadastrar: ");
+                String nomeTarefa = leitorTexto.nextLine();
+                tarefas.add(nomeTarefa);
+                System.out.println("Tarefa: " + nomeTarefa + " adicionado!");
+            }
+
+            if (escolha == 2) {
+                System.out.println("Lista de tarefas: ");
+                for (int i = 0; i < tarefas.size(); i++) {
+                    System.out.println(i+1 + " - " + tarefas.get(i));
+                }
+            }
+
+            if (escolha == 3) {
+                System.out.println("Marcar como concluida..");
+            }
+
+        }
+        return tarefas;
+    }
+
+}
---
+++ Henrique/src/main/java/flow/infra/Main.java
@@ -0,0 +1,11 @@
+package flow.infra;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GerenciadorDeTarefas tarefa = new GerenciadorDeTarefas();
+        tarefa.cadastrarTarefas();
+
+    }
+}
